26ba509fd45be99e665a766fa572f22ef0531a59
yis-park <ckadltmf423@naver.com>
2023-02-10 17:57:46 +0900

    모달수정

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+// import logImg from "../assets/images/longin.png";
 
 function Modal(props) {
   const Container = styled.div`
@@ -8,10 +9,17 @@ function Modal(props) {
     top: 56px;
     right: 100px;
   `;
+  // const Img = styled.div`
+  //   width: 22px;
+  //   height: 22px;
+  //   vertical-align: bottom;
+  //   margin-left: 16px;
+  // `;
 
   return (
     <Container className="madal">
-      <p>로그인이 필요한 페이지입니다.</p>
+      {/* <img className="logImg" src={logImg} /> */}
+      <p>즐겨찾기 기능은 로그인이 필요해요.</p>
       <p>회원가입 먼저 진행해주세요.</p>
       <Link to="/join"> 회원가입 하러가기</Link>
     </Container>
